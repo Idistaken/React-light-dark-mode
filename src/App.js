f52ed89f20ad7db6f3e775ce443d3d7a79ae842b
Idistaken <np67440@gmail.com>
2022-06-22 13:40:44 +0530

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Box from "@mui/material/Box";
+import { useContext } from "react";
+import { ColorModeContext } from "./context/ColorModeContext.jsx";
+import { useTheme } from "@mui/material/styles";
+import Button from "@mui/material/Button";
+import { Stack } from "@mui/material";
 
 function App() {
+  const theme = useTheme();
+  const colorMode = useContext(ColorModeContext);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Box
+        sx={{
+          display: "flex",
+          width: "100%",
+          alignItems: "center",
+          justifyContent: "center",
+          bgcolor: "background.default",
+          color: "text.primary",
+          borderRadius: 1,
+          p: 3,
+        }}
+      >
+        <Stack direction="column">
+          <Button
+            sx={{ ml: 1, fontWeight: "regular", fontSize: "3em" }}
+            onClick={colorMode.toggleColorMode}
+            color="inherit"
+          >
+            {theme.palette.mode}
+          </Button>
+
+          <Button
+            sx={{
+              ml: 1,
+              fontWeight: "regular",
+              fontSize: "3em",
+            }}
+            color="inherit"
+            disabled
+          >
+            Disabled
+          </Button>
+        </Stack>
+      </Box>
     </div>
   );
 }
